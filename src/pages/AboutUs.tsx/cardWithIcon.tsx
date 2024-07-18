e14f0d4fbe223f914b78bea2a0dc316a8774cd4d
Ruth-Musasa <ruthebondo2001@gmail.com>
2024-07-18 12:56:00 +0200

    responsive du composant about us

--- src/pages/AboutUs.tsx/cardWithIcon.tsx
+++ src/pages/AboutUs.tsx/cardWithIcon.tsx
@@ -9,13 +9,13 @@ const CardWithIcon: React.FC<Props> = ({ img, children }) => {
     return (
         <div className="w-11/12 m-auto">
             <div className="py-16 h-auto">
-                <div className="w-11/12  m-auto flex flex-col md:flex-row gap-8 2xl:gap-32">
+                <div className="w-11/12  m-auto flex flex-col lg:flex-row gap-8 2xl:gap-32">
                     <div className=" space-y-10 w-full md:w-10/12 bottom-0 left-0 m-auto">
                         <div>
                             {children}
                         </div>
                     </div>
-                    <img src={img} className="md:w-5/12 md:h-full md:m-auto 2xl:w-2/3" alt="" />
+                    <img src={img} className="lg:w-5/12 md:h-full md:m-auto 2xl:w-2/3" alt="" />
                 </div>
             </div>
         </div>
