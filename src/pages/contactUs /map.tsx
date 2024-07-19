90d20abd875ba7a07cb22a21b6fb7250e69e1e25
Ruth-Musasa <ruthebondo2001@gmail.com>
2024-07-19 17:42:52 +0200

    uptdate

--- src/pages/contactUs /map.tsx
+++ src/pages/contactUs /map.tsx
@@ -1,5 +1,31 @@
+// import React from 'react';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import 'leaflet/dist/leaflet.css';
+
+// const center = {
+//   lat: -11.6869,
+//   lng: 27.4857,
+// };
+
+// function Map() {
+//   return (
+//     <MapContainer center={center} zoom={15} style={{ width: '100%', height: '750px' }}>
+//       <TileLayer
+//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       />
+//       <Marker position={center}>
+//         <Popup>
+//           Avenue Fatuma, Lubumbashi <br /> (9F52+WRQ)
+//         </Popup>
+//       </Marker>
+//     </MapContainer>
+//   );
+// }
+
+// export default React.memo(Map);
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const center = {
@@ -9,11 +35,11 @@ const center = {
 
 function Map() {
   return (
-    <MapContainer center={center} zoom={15} style={{ width: '100%', height: '750px' }}>
-      <TileLayer
+    <MapContainer  style={{ width: '100%', height: '750px' }}>
+      {/* <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+      /> */}
       <Marker position={center}>
         <Popup>
           Avenue Fatuma, Lubumbashi <br /> (9F52+WRQ)
