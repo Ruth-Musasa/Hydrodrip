f79053f6c25fa4ea60406adb2cff2fbc12e98b7a
Ruth-Musasa <ruthebondo2001@gmail.com>
2024-07-25 12:16:13 +0200

    update de la map

--- src/pages/contactUs /map.tsx
+++ src/pages/contactUs /map.tsx
@@ -1,31 +1,5 @@
-// import React from 'react';
-// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-// import 'leaflet/dist/leaflet.css';
-
-// const center = {
-//   lat: -11.6869,
-//   lng: 27.4857,
-// };
-
-// function Map() {
-//   return (
-//     <MapContainer center={center} zoom={15} style={{ width: '100%', height: '750px' }}>
-//       <TileLayer
-//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//       />
-//       <Marker position={center}>
-//         <Popup>
-//           Avenue Fatuma, Lubumbashi <br /> (9F52+WRQ)
-//         </Popup>
-//       </Marker>
-//     </MapContainer>
-//   );
-// }
-
-// export default React.memo(Map);
 import React from 'react';
-import { MapContainer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 const center = {
@@ -35,11 +9,11 @@ const center = {
 
 function Map() {
   return (
-    <MapContainer className='w-full h-96 md:h-[700px] '>
-      {/* <TileLayer
+    <MapContainer className='w-full h-96 md:h-[650px] ' center={center} zoom={15} >
+      <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      /> */}
+      />
       <Marker position={center}>
         <Popup>
           Avenue Fatuma, Lubumbashi <br /> (9F52+WRQ)
@@ -50,3 +24,30 @@ function Map() {
 }
 
 export default React.memo(Map);
+
+// import React from 'react';
+// import { MapContainer, Marker, Popup } from 'react-leaflet';
+// import 'leaflet/dist/leaflet.css';
+
+// const center = {
+//   lat: -11.6869,
+//   lng: 27.4857,
+// };
+
+// function Map() {
+//   return (
+//     <MapContainer className='w-full h-96 md:h-[700px] '>
+//       {/* <TileLayer
+//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       /> */}
+//       <Marker position={center}>
+//         <Popup>
+//           Avenue Fatuma, Lubumbashi <br /> (9F52+WRQ)
+//         </Popup>
+//       </Marker>
+//     </MapContainer>
+//   );
+// }
+
+// export default React.memo(Map);
